dc03b1a51307d0e378037eaa4b671f47784c9c71
Luke Texon <luk.tex37@gmail.com>
2022-07-15 00:37:34 +0100

    new test

--- src/apps/tpcc/app/tpcc.cpp
+++ src/apps/tpcc/app/tpcc.cpp
@@ -137,15 +137,17 @@ namespace ccfapp
         auto districts_table = args.tx.ro(tpcc::TpccTables::districts);
         auto optional_district = districts_table->get(key);
 
-//        if (true)
-//        {
-//          throw std::logic_error("district does not exist");
-//        }
-//
-//        auto response = optional_district.value();
-//
-//        set_ok_status(args);
-//        args.rpc_ctx->set_response_body(response.serialize());
+        if (true)
+        {
+          set_error_status(args, HTTP_STATUS_BAD_REQUEST, "district does not exist")
+        }
+        else
+        {
+          auto response = optional_district.value();
+
+          set_ok_status(args);
+          args.rpc_ctx->set_response_body(response.serialize());
+        }
       };
 
       auto get_order_line = [this](auto& args) {
