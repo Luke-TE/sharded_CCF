5eb600fdb883c595e8e36608b008b3d2e04b10b6
Luke Texon <luk.tex37@gmail.com>
2022-07-15 15:07:00 +0100

    [ljt18]: comment

--- src/apps/tpcc/clients/tpcc_client.cpp
+++ src/apps/tpcc/clients/tpcc_client.cpp
@@ -187,15 +187,15 @@ private:
     //         keys_deleted;
     // TODO execute transaction
 
-    if (!options.no_wait)
-    {
-      auto c = create_connection(true, false);
-      wait_for_global_commit(last_response_tx_id);
-    }
-    const auto last_commit = last_response_tx_id.seqno;
-    auto timing_results = end_timing(last_commit);
-    LOG_INFO_FMT("Timing ended");
-    return timing_results;
+//    if (!options.no_wait)
+//    {
+//      auto c = create_connection(true, false);
+//      wait_for_global_commit(last_response_tx_id);
+//    }
+//    const auto last_commit = last_response_tx_id.seqno;
+//    auto timing_results = end_timing(last_commit);
+//    LOG_INFO_FMT("Timing ended");
+//    return timing_results;
   }
 
 
