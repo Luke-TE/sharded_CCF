86d64e7211f0bffa788ecea86a2ba2f772cc86e4
Luke Texon <luk.tex37@gmail.com>
2022-07-15 12:37:18 +0100

    add num ints

--- src/apps/tpcc/app/tpcc.cpp
+++ src/apps/tpcc/app/tpcc.cpp
@@ -354,6 +354,7 @@ namespace ccfapp
         ints.push_back(456);
 
         tpcc::TestVectorStruct test_vector_struct;
+        test_vector_struct.num_ints = 0;
         test_vector_struct.ints = ints;
 
         set_ok_status(args);
